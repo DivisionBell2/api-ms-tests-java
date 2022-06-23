c98bdf3f051997e0e8aee714d7cf68ff798ede56
DivisionBell2 <32009088+DivisionBell2@users.noreply.github.com>
2022-06-23 17:43:46 +0300

    Add files via upload

---
+++ src/main/java/ru/projects/entities/orbCommon/json/address/FullRegistryAddressJson.java
@@ -0,0 +1,14 @@
+
+package ru.project.entities.orbCommon.json.address;
+
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class FullRegistryAddressJson {
+
+    private AddressJson fullRegistryAddress;
+
+}
